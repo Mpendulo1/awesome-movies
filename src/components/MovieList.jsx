8335ea03021647040ad5034fe25441ff2f6dabc0
mpendulo1 <mpendulokhozamk2@gmail.com>
2021-12-17 15:20:26 +0200

    add infinite scroll feature

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 import { Link, useMatch } from "react-router-dom";
 import axios from "axios";
+import InfiniteScroll from 'react-infinite-scroll-component'; 
 
 import MovieCard from "./MovieCard";
 
 function MovieList() {
     const [movies, setMovies] = useState([]);
+    const [pageCount, setPageCount] = useState(1);
 
     const fetchMovies = async () => { 
         try {
             let response = await axios({
                    method: "get",
-                   url: "https://api.themoviedb.org/3/movie/popular?api_key=d4f7b87d7cedfdfbbb297f46aa3e8779&language=en-US&page=1"
+                   url: `https://api.themoviedb.org/3/movie/popular?api_key=d4f7b87d7cedfdfbbb297f46aa3e8779&language=en-US&page=${pageCount}`
             });
             
             const data = await response.data.results;
-            setMovies(data);
+            setMovies([...movies, ...data]);
+            setPageCount( pageCount + 1);
      } catch (error) {
             console.log(error);
      }
@@ -27,6 +30,19 @@ function MovieList() {
  
     return( 
         <ul className="movie-list">
+        <InfiniteScroll
+         className="movie-list"
+            dataLength={movies.length} //This is important field to render the next data
+            next={fetchMovies}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+                <p style={{ textAlign: 'center' }}>
+                <b>Yay! You have seen it all</b>
+                </p>
+            }
+            >
+                
         {
             movies.map( movie => (
                 <li key={movie.id}>
@@ -40,6 +56,7 @@ function MovieList() {
               </li>
             ))
         }
+        </InfiniteScroll>
         </ul>
     );
 }
